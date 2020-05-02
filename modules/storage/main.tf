fecc19a315994775d2054e998a039d30d2480df4
kentatogashi <kenta.togashi@example.com>
2020-05-02 20:34:10 +0900

    Replace storage main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,27 +1,18 @@
-resource "google_compute_network" "vpc" {
-  name = var.vpc_name.data
-  auto_create_subnetworks = false
+resource "google_storage_bucket" "storage" {
+  name = var.bucket_name
+  location = var.region
+  storage_class = "REGIONAL"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      num_newer_versions = 5
+    }
+  }
 }
-
-resource "google_compute_subnetwork" "togashi-veri-subnet01" {
-  name = "togashi-veri-subnet01"
-  ip_cidr_range = "192.168.10.0/24"
-  region = "asia-northeast1"
-  network = google_compute_network.vpc.self_link
-}
-
-resource "google_compute_subnetwork" "togashi-veri-subnet02" {
-  name = "togashi-veri-subnet02"
-  ip_cidr_range = "192.168.20.0/24"
-  region = "asia-northeast1"
-  network = google_compute_network.vpc.self_link
-}
-
-resource "google_compute_subnetwork" "togashi-veri-subnet03" {
-  name = "togashi-veri-subnet03"
-  ip_cidr_range = "192.168.30.0/24"
-  region = "asia-northeast1"
-  network = google_compute_network.vpc.self_link
-}
-
-
