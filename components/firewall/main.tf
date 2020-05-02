b0b596dffe617c19dc546f91731b2436d54a1131
kentatogashi <kenta.togashi@example.com>
2020-05-02 23:57:52 +0900

    Update firewall

--- components/firewall/main.tf
+++ components/firewall/main.tf
@@ -10,3 +10,16 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags = ["bastion-server"]
   source_ranges = var.src_ips
 }
+
+resource "google_compute_firewall" "allow-http" {
+  name = "togashi-${var.env}-firewall-allow-http"
+  network = "togashi-${var.env}-vpc01"
+
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+
+  target_tags = ["http-server"]
+  source_tags = ["bastion-server"]
+}
