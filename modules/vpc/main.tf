0fff58f25fea506000e47ff59d01f3cea22f6399
kentatogashi <kenta.togashi@example.com>
2020-05-02 19:31:43 +0900

    Add subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,3 +3,25 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "togashi-veri-subnet01" {
+  name = "togashi-veri-subnet01"
+  ip_cidr_range = "192.168.10.0/24"
+  region = "asia-northeast1"
+  network = google_compute_network.vpc.self_link
+}
+
+resource "google_compute_subnetwork" "togashi-veri-subnet02" {
+  name = "togashi-veri-subnet02"
+  ip_cidr_range = "192.168.20.0/24"
+  region = "asia-northeast1"
+  network = google_compute_network.vpc.self_link
+}
+
+resource "google_compute_subnetwork" "togashi-veri-subnet03" {
+  name = "togashi-veri-subnet03"
+  ip_cidr_range = "192.168.30.0/24"
+  region = "asia-northeast1"
+  network = google_compute_network.vpc.self_link
+}
+
+
