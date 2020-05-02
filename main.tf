5ba25e5c8a4302aa0cec4daed0bb5d5d8e9f24cf
kentatogashi <kenta.togashi@example.com>
2020-05-02 21:44:23 +0900

    Rename modules directory

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "google" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./components/vpc"
   env = var.env
   region = var.region
   cidrs = var.cidrs
 }
 
 module "storage" {
-  source = "./modules/storage"
+  source = "./components/storage"
   env = var.env
   region = var.region
 }
